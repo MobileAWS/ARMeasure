264e1d80f429612cc213dc0c73ebd0a82892a53c
Hans Ospina <hansospina@gmail.com>
2018-02-04 16:06:13 -0500

    Initial Commit

---
+++ ARDistance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARDistance
+//
+//  Created by Hans Ospina on 2/4/18.
+//  Copyright © 2018 MobileAWS, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
